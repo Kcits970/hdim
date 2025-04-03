6a83a060b7354ffc3e193459a4a886fd3efdb561
최승열 <a2teosis@naver.com>
2025-04-03 16:29:00 +0900

    choi option

--- src/main.c
+++ src/main.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 #include "dump.h"
+#include "options.h"
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-		goto bad_usage;
+    int dump_mode = 0, char_mode = 0, version_mode = 0;
+    char *output_file = NULL;
 
-	FILE *f = fopen(argv[1], "r");
-	if (!f)
-	{
-		fprintf(stderr, "./hdim: ");
-		perror(argv[1]);
-		return 1;
-	}
+    if (argc < 2)
+        goto bad_usage;
 
-	dump(f);
+    for (int i = 2; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-d") == 0)
+            dump_mode = 1;
+        else if (strcmp(argv[i], "-c") == 0)
+            char_mode = 1;
+        else if (strcmp(argv[i], "-V") == 0)
+            version_mode = 1;
+        else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc)
+            output_file = argv[++i];
+        else
+            goto bad_usage;
+    }
 
-	fclose(f);
-	return 0;
+    FILE *f = fopen(argv[1], "r");
+    if (!f)
+    {
+        fprintf(stderr, "./hdim: ");
+        perror(argv[1]);
+        return 1;
+    }
+
+    if (dump_mode)
+        handle_option_d(f);
+    if (char_mode)
+        handle_option_c(f);
+    if (version_mode)
+        handle_option_v();
+    if (output_file)
+        handle_option_o(f, output_file);
+
+    fclose(f);
+    return 0;
 
 bad_usage:
-	fprintf(stderr, "Usage: ./hdim <file>\n");
-	return 1;
+    fprintf(stderr, "Usage: ./hdim <file> [-d] [-c] [-V] [-o output_file]\n");
+    return 1;
 }
---
+++ src/option_v.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void handle_option_v()
+{
+    printf("hdim version 1.0.0\n");
+}
