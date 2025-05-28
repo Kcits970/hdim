d776df6c1fe9dd47d15f98f83077f87422f7b463
Kcits970 <geometricstick@gmail.com>
2025-05-28 11:05:01 +0900

    added utility header file (includes min, max functions)

---
+++ src/util.h
@@ -0,0 +1,14 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+static inline int imin2(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+static inline int imax2(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+#endif
